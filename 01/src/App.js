76578ce69a83f1d7932f4c32d767d17ffd8537e1
pokusaeff.daniil <69844034+iwannabekoshka@users.noreply.github.com>
2021-06-15 18:34:15 +0300

    01. композиция, компоненты-обертки

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses"
 
 function App() {
     const expenses = [
@@ -30,11 +31,7 @@ function App() {
     return (
         <div>
             <h2>Let's get koshka</h2>
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
+            <Expenses expenses={expenses}/>
         </div>
     );
 }
