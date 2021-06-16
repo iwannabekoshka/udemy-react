5d3d45bd1d22834b37ac8e9c68b4b996a85ac933
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-06-16 15:32:17 +0300

    01. фильтр по годам (пока без фильтра, но с передачей года в блок с расходами)

--- 01/src/components/Expenses/Expenses.js
+++ 01/src/components/Expenses/Expenses.js
@@ -1,27 +1,34 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import React from "react";
 
 function Expenses(props) {
-	return (
-		<Card className="expenses">
-			<ExpenseItem
-				title={props.expenses[0].title}
-				amount={props.expenses[0].amount}
-				date={props.expenses[0].date}
-			/>
-			<ExpenseItem
-				title={props.expenses[1].title}
-				amount={props.expenses[1].amount}
-				date={props.expenses[1].date}
-			/>
-			<ExpenseItem
-				title={props.expenses[2].title}
-				amount={props.expenses[2].amount}
-				date={props.expenses[2].date}
-			/>
-		</Card>
-	)
+    const changeYearHandler = (year) => {
+        console.log(year)
+    }
+
+    return (
+        <Card className="expenses">
+            <ExpensesFilter onChangeYear={changeYearHandler}/>
+            <ExpenseItem
+                title={props.expenses[0].title}
+                amount={props.expenses[0].amount}
+                date={props.expenses[0].date}
+            />
+            <ExpenseItem
+                title={props.expenses[1].title}
+                amount={props.expenses[1].amount}
+                date={props.expenses[1].date}
+            />
+            <ExpenseItem
+                title={props.expenses[2].title}
+                amount={props.expenses[2].amount}
+                date={props.expenses[2].date}
+            />
+        </Card>
+    )
 }
 
 export default Expenses;
