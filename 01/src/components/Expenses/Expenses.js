b62ae05caa421eb8bdbd4df5b88417fad4223d5c
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-06-17 07:59:48 +0300

    01. рендер списка

--- 01/src/components/Expenses/Expenses.js
+++ 01/src/components/Expenses/Expenses.js
@@ -16,21 +16,13 @@ function Expenses(props) {
     return (
         <Card className="expenses">
             <ExpensesFilter onChangeYear={changeYearHandler} year={year}/>
-            <ExpenseItem
-                title={props.expenses[0].title}
-                amount={props.expenses[0].amount}
-                date={props.expenses[0].date}
-            />
-            <ExpenseItem
-                title={props.expenses[1].title}
-                amount={props.expenses[1].amount}
-                date={props.expenses[1].date}
-            />
-            <ExpenseItem
-                title={props.expenses[2].title}
-                amount={props.expenses[2].amount}
-                date={props.expenses[2].date}
-            />
+            {props.expenses.map(expense =>
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            )}
         </Card>
     )
 }
