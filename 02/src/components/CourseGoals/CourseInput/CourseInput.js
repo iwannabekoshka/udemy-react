2b63aca9b375575de5896a5a878451fb5061f872
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-06-22 13:01:10 +0300

    02. динамические классы

--- 02/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const invalidClass = isValid ? '' : 'invalid';
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    const goalInputChangeHandler = event => {
+        setEnteredValue(event.target.value);
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className={`form-control ${invalidClass}`}>
+                <label>Course Goal</label>
+                <input
+                    type="text"
+                    onChange={goalInputChangeHandler}
+                />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
