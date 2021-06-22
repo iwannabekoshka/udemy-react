6526ef2d0fdb3f605a6ce859bf603fbe7000ef37
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-06-22 14:21:21 +0300

    02. styled-components 2

--- 02/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,32 +8,32 @@ const FormControl = styled.div`
     margin: 0.5rem 0;
     
     & label {
-      font-weight: bold;
-      display: block;
-      margin-bottom: 0.5rem;
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
     }
     
     & input {
-      display: block;
-      width: 100%;
-      border: 1px solid #ccc;
-      font: inherit;
-      line-height: 1.5rem;
-      padding: 0 0.25rem;
+        display: block;
+        width: 100%;
+        border: 1px solid #ccc;
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
     }
     
     & input:focus {
-      outline: none;
-      background: #fad0ec;
-      border-color: #8b005d;
+        outline: none;
+        background: #fad0ec;
+        border-color: #8b005d;
     }
     
     &.invalid label {
-      color: red;
+        color: red;
     }
     &.invalid input {
-      background-color: salmon;
-      border-color: red;
+        background-color: salmon;
+        border-color: red;
     }
 `;
 
